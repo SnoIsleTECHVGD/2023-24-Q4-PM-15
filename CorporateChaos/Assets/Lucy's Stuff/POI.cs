d559d456cc94d24510d9479b786ce86e203f0c1b
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-03-21 12:10:58 -0700

    Legit nothing

--- CorporateChaos/Assets/Lucy's Stuff/POI.cs
+++ CorporateChaos/Assets/Lucy's Stuff/POI.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +8,7 @@ public class POI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        print("SHLATT");
     }
 
     // Update is called once per frame
