559dfced06c93990ec4c2e9c9a2191e646a863c0
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-03-21 12:39:12 -0700

    File changes

--- CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/POI.cs
+++ CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/POI.cs
@@ -8,7 +8,7 @@ public class POI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        print("SHLATT");
+        
     }
 
     // Update is called once per frame
