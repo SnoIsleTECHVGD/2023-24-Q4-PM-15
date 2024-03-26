4491d6ec3702dfe1f8dbb5ff6ae3af8832c979c3
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-03-26 13:35:33 -0700

    More Vending Work

--- CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/Scripts/VendingMachine.cs
+++ CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/Scripts/VendingMachine.cs
@@ -4,9 +4,11 @@ using System.Threading;
 using System.Threading.Tasks;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class VendingMachine : MonoBehaviour
 {
+    [SerializeField] public Animator animator;
     private void Update()
     {
         if (Input.GetMouseButtonDown(0))
@@ -14,33 +16,48 @@ public class VendingMachine : MonoBehaviour
             StartCoroutine(Size());
 
             IncreaseKick();
-        }    
-       
+
+            animator.SetBool("isplayimg, true");
+            //ImageChange();
+        }
+
 
     }
 
     public GameObject Machinekick;
     public double CurrentKickCount { get; set; }
     public GameObject FoodGiver;
+    public Image NormalImage;
+    public Sprite HurtImage;
 
-public void OnMachinekick()
+    public void OnMachinekick()
     {
         IncreaseKick();
         FoodGiver.transform.localScale = new Vector3(1f, 1f, 1f);
     }
     public void IncreaseKick()
     {
-        CurrentKickCount += 1;
+        CurrentKickCount += .5f;
         print(CurrentKickCount);
     }
-    
+
     public IEnumerator Size()
     {
-        RectTransform rectTransform = GetComponent<RectTransform>();
-        rectTransform.Rotate(new Vector3(0, 0, 3));
+        // RectTransform rectTransform = GetComponent<RectTransform>();
+        // rectTransform.Rotate(new Vector3(0, 0, 3));
         FoodGiver.transform.localScale = new Vector3(1f, 1f, 1f);
         yield return new WaitForSeconds(.3f);
         FoodGiver.transform.localScale = new Vector3(.5f, .5f, .5f);
     }
-     
+
+        public void Animations()
+    {
+        animator.SetBool("isplaying", true);
+    }
+  //  public void ImageChange()
+   // {
+   //     NormalImage.sprite = HurtImage;
+       // yield return new WaitForSeconds(.3f);
+      //  HurtImage = NormalImage.sprite;
+   // }
 }
