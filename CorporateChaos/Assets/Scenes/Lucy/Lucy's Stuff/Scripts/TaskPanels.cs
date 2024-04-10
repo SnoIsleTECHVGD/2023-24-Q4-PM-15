83282858f833a24e60927320140a17b100308f24
MUKILTEO\1717960 <1717960@mukilteo.wednet.edu>
2024-04-10 13:35:32 -0700

    Haunted copy

--- CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/Scripts/TaskPanels.cs
+++ CorporateChaos/Assets/Scenes/Lucy/Lucy's Stuff/Scripts/TaskPanels.cs
@@ -5,12 +5,34 @@ using UnityEngine;
 public class TaskPanels : MonoBehaviour
 {
    public GameObject TaskPanel = null;
-    private bool taskPanelOpened = false;
-      
+    private bool taskPanelOpened = true;
+    private bool inRadius;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        inRadius = true;
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        inRadius = false;
+    }
+    private void Update()
+    {
+        
+        if (inRadius == true && Input.GetKeyDown(KeyCode.E)) 
+
+            {
+          
+                taskPanelOpened = true;
+                ToggleTaskPanel();
+            }
+    }
+    
     public void ToggleTaskPanel()
     {
         taskPanelOpened = !taskPanelOpened;
         SetTaskPanelVisibility();
+        Debug.Log("Helppppp");
     }
     private void SetTaskPanelVisibility()
     {
