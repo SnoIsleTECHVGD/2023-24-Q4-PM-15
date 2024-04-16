1c0fb5df6200720f29dc00794cb31e0d62f85b47
MUKILTEO\1717860 <1717860@mukilteo.wednet.edu>
2024-04-16 13:38:59 -0700

    Animations and collider fixed.

--- CorporateChaos/Assets/Scenes/Dylan/Scripts/movement.cs
+++ CorporateChaos/Assets/Scenes/Dylan/Scripts/movement.cs
@@ -1,34 +1,67 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.ReorderableList;
 using UnityEngine;
 
 public class movement : MonoBehaviour
 {
     public KeyCode left, right, up, down;
     public float moveSpeed;
+    float horizontalInput;
+    float verticalInput;
+    bool isFacingRight = false;
 
+    Vector2 moveing;
+
+    Animator playerAnimator;
     public Rigidbody2D rb2D;
-    Vector2 moving;
+
 
     // Update is called once per frame
+    private void Start()
+    {
+        playerAnimator = GetComponent<Animator>();
+    }
+
     void Update()
     {
-        moving.x = Input.GetAxisRaw("Horizontal");
-        moving.y = Input.GetAxisRaw("Vertical");
+        moveing.x = Input.GetAxisRaw("Horizontal");
+        moveing.y = Input.GetAxisRaw("Vertical");
+
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        FlipSprite();
 
         GetComponent<Animator>().SetInteger("WalkDirection", 0);
 
-       //hook up animations later
-       if (Input.GetKey(left))
+        //hook up animations later
+        if (Input.GetKey(left))
         {
             GetComponent<Animator>().SetInteger("WalkDirection", 1);
         }
-
-     
+        if (Input.GetKey(right)) 
+        {
+            GetComponent<Animator>().SetInteger("WalkDirection", 1);
+        }
     }
 
     void FixedUpdate()
     {
-        rb2D.MovePosition(rb2D.position + moving * moveSpeed * Time.fixedDeltaTime);
+        rb2D.MovePosition(rb2D.position + moveing * moveSpeed * Time.fixedDeltaTime);
+        playerAnimator.SetFloat("xVelocity", Mathf.Abs(rb2D.velocity.x));
+        playerAnimator.SetFloat("yVelocity", rb2D.velocity.y);
+    }
+
+    void FlipSprite()
+    {
+        if(isFacingRight && horizontalInput < 0f || !isFacingRight && horizontalInput > 0f)
+        {
+            isFacingRight = !isFacingRight;
+            Vector3 ls = transform.localScale;
+            ls.x *= -1f;
+            transform.localScale = ls;
+        }
     }
 }
+    
