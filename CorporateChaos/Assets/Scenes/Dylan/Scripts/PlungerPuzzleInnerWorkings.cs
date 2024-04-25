37981f096dce30a2bdcfcd2fbb917af414413f05
MUKILTEO\1717860 <1717860@mukilteo.wednet.edu>
2024-04-25 12:22:27 -0700

    Pushed stuff to master scene.

---
+++ CorporateChaos/Assets/Scenes/Dylan/Scripts/PlungerPuzzleInnerWorkings.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class PlungerPuzzleInnerWorkings : MonoBehaviour
+{
+    Vector2 difference = Vector2.zero;
+    public GameObject Toilet;
+    public bool unClogged = false;
+    public int Counter = 0;
+
+    private void OnMouseDown()
+    {
+        difference = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - (Vector2)transform.position;
+    }
+
+    private void OnMouseDrag()
+    {
+        transform.position = (Vector2)Camera.main.ScreenToWorldPoint(Input.mousePosition) - difference;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Water.GetComponent<Renderer>().enabled = false;
+        Counter = Counter + 1;
+
+        if (Counter == 10)
+        {
+            unClogged = true;
+        }
+
+        if (unClogged == true)
+        {
+            Debug.Log("toilet is unclogged ");
+            Destroy(collision.gameObject);
+        }
+    }
+}
